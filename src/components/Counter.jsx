1c295c1718e8ed211fc78b671ed379e148eddcb7
Myint Myat Pyae Sone <myintmyatps664@gmail.com>
2024-06-05 00:24:13 +0630

    Final Commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,41 @@
+import {useSelector, useDispatch} from "react-redux";
+
 const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const isShow = useSelector(state => state.isShow);
+    const dispatch = useDispatch();
+
+    const increaseHandler = () => {
+        dispatch({ type: "increase"});
+    }
+    const decreaseHandler = () => {
+        dispatch({ type: "decrease"});
+    }
+    const increaseMoreHandler = () => {
+        dispatch({type: "increaseMore" , amount: 5});
+    }
+    const counterHandler = () => {
+        dispatch({type: "toggleCounter"})
+    }
+
   return (
     <section>
       <h2>Redux Counter</h2>
-      <h1>0</h1>
+      {
+        isShow && <h1>{counter}</h1>
+      }
       <hr />
       <div className="btn-div">
-        <button>Increase</button>
-        <button>Increase By 5</button>
-        <button>Decrease</button>
+        <button onClick={increaseHandler}>Increase</button>
+        <button onClick={increaseMoreHandler}>Increase By 5</button>
+        <button onClick={decreaseHandler}>Decrease</button>
+        {
+            isShow && <button onClick={counterHandler}>Hide Counter</button>
+        }
+        {
+            !isShow && <button onClick={counterHandler}>Show Counter</button>
+        }
       </div>
     </section>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,40 @@
 /* eslint-disable no-unused-vars */
-import {legacy_createStore as createStore} from "redux";
+import { legacy_createStore as createStore } from "redux";
 
-const counterReducer = (state = {counter: 0}, action) => {
-    return state;
-}
+const initialState = { counter: 0, isShow: true };
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + 1,
+      isShow: state.isShow,
+    };
+  }
+  else if (action.type === "decrease") {
+    if(state.counter > 1){
+        return {
+          counter: state.counter - 1,
+          isShow: state.isShow,
+        };
+    }
+    else{
+        alert("You cant decrease number below 0");
+    }
+  }
+  else if (action.type === "increaseMore") {
+    return {
+      counter: state.counter + action.amount,
+      isShow: state.isShow,
+    };
+  }
+  else if (action.type === "toggleCounter") {
+    return {
+      isShow: !state.isShow,
+      counter: state.counter,
+    };
+  }
+
+  return state;
+};
 const store = createStore(counterReducer);
 
 export default store;
